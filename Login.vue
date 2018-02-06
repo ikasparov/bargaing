61627357a0057edbbcb879f9dfa665af9761abc5
DIma__S <hevillds@gmail.com>
2018-02-06 14:56:02 +0300

    Сделали заглушки

---
+++ Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <body>
+    <header>
+      <h1>Добро пожаловать!</h1>
+    </header>
+
+    <section>
+      <span>Вы перешли на страницу авторизации, но, к сожалению, она еще не готова</span>\
+      <p>
+        В будущем на данной странице Вы сможете войти в свой аккаунт
+      </p>
+
+      <p>
+        А затем пользоваться функционалом сайта!
+      </p>
+    </section>
+
+
+    <footer>
+      <p>
+        <b>Осталось совсем немного!</b>
+      </p>
+    </footer>
+    <link rel="stylesheet" href="../src/styles/blankets/blanket1.scss">
+    <router-view></router-view>
+  </body>
+</template>
--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
-import './styles/styles.scss';
+'use strict';
 
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter);
 
+// const home = require('./home.vue').default;
+const Login = require('./Login.vue').default;
+const Reg = require('./Reg.vue').default;
 
+const routes = [
+    // { path: '/home', component: home },
+    { path: '/login', component: Login },
+    { path: '/reg', component: Reg }
+  ];
+
+const router = new VueRouter({
+  routes
+});
+
+const app = new Vue({
+  router
+}).$mount('#app');
