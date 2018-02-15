d299bd9a132c2edbaf012b6571184b4b9460b7e0
ikasparov <ikasparov.dev@gmail.com>
2018-02-15 20:37:22 +0300

    Add reserve room api endpoint

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,25 @@
 class RoomsController < ApplicationController
   def index
     render json: { rooms: Room.all.as_json(only: [:id, :name]) }
+    # @rooms = Room.all
+    # if params[:date]
+    #   @schedules = 
+    # end
+  end
+
+  def reserve
+    schedule = Schedule.new schedule_permit
+
+    if schedule.save
+      render json: schedule.as_json(only: [:id, :date, :hour])
+    else
+      render json: schedule.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def schedule_permit
+    params.require(:schedule).permit(:room_id, :user_id, :date, :hour)
   end
 end
